3f146fd40646c78cff1bfd49ccd2f9ec2eeef1e1
Varshini Yanamandra <45843829+yvarshini@users.noreply.github.com>
2023-04-17 19:18:23 -0400

    updated search_app.ipynb and exceptions.py
    - added code to search based on location and tweets by a specific user
    - added sort parameter to functions where it is applicable
    - added exception class for invalid sort criterion

--- exceptions.py
+++ exceptions.py
@@ -7,3 +7,7 @@ class UserNotFoundError(Exception):
 class TweetNotFoundError(Exception):
     code = 404
     description = {"error message": "The tweet specified using the tweet ID was not found in the database. Please check and try again."}
+
+class InvalidSortCriterionError(Exception):
+    code = 400
+    description = {"error message": "The sort criterion specified is invalid. If you wish to sort your results, please specify one of 'oldestToNewest', 'newestToOldest' or 'popularity'."}
